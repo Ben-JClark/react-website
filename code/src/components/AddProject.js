2b71de378d4a35b16a4b740eced6615c7bec9f8b
Ben-JClark <benclarkat@proton.me>
2023-08-14 07:58:06 +1200

    AddProjects comp moved to seperate js file

---
+++ code/src/components/AddProject.js
@@ -0,0 +1,104 @@
+import Form from "react-bootstrap/Form";
+import Modal from "react-bootstrap/Modal";
+import React, { useRef } from "react";
+import Button from "react-bootstrap/Button";
+
+// Displays A Bootstrap Modal contains a form to submit form information
+const AddProjects = (props) => {
+  //references to all the textboxes in the form
+  const projectIDRef = useRef(null);
+  const projectNameRef = useRef(null);
+  const projectDscrRef = useRef(null);
+  const projectStartRef = useRef(null);
+  const projectEndRef = useRef(null);
+
+  //When the form is submitted handle adding a new project to the list
+  const handleAdd = (e) => {
+    e.preventDefault();
+    console.log("Adding project");
+
+    //Create a new project using the textbox input
+    let newProject = {
+      projectIdentifier: projectIDRef.current.value,
+      projectName: projectNameRef.current.value,
+      description: projectDscrRef.current.value,
+      start_date: projectStartRef.current.value,
+      end_date: projectEndRef.current.value,
+    };
+
+    //toggle the form to be hidden
+    props.handleClose();
+    //add the project to the list
+    props.addProject(newProject);
+  };
+
+  //Return a bootstrap modal contianing a form to update the project list
+  return (
+    <div>
+      <div
+        className="modal show"
+        style={{ display: "block", position: "initial" }}
+      >
+        <Modal show={props.show} onHide={props.handleClose}>
+          <Modal.Header>
+            <Modal.Title>Add Project</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group className="mb-3" controlId="formId">
+                <Form.Label>Project ID</Form.Label>
+                <Form.Control
+                  type="text"
+                  ref={projectIDRef}
+                  placeholder="e.g. 17"
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formName">
+                <Form.Label>Project Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  ref={projectNameRef}
+                  placeholder="e.g. SQL Database"
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formDscr">
+                <Form.Label>Project Description</Form.Label>
+                <Form.Control
+                  type="text"
+                  ref={projectDscrRef}
+                  placeholder="e.g. Stores user data"
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formStart">
+                <Form.Label>Project Start Date</Form.Label>
+                <Form.Control
+                  type="text"
+                  ref={projectStartRef}
+                  placeholder="YYYY-MM-DD HH:MM:SS"
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formEnd">
+                <Form.Label>Project End Date</Form.Label>
+                <Form.Control
+                  type="text"
+                  ref={projectEndRef}
+                  placeholder="YYYY-MM-DD HH:MM:SS"
+                />
+              </Form.Group>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={props.handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={handleAdd}>
+              Add Project
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    </div>
+  );
+};
+
+export default AddProjects;
--- code/src/components/App.js
+++ code/src/components/App.js
@@ -1,13 +1,12 @@
 import "../css/App.css";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { without } from "lodash";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
-import Modal from "react-bootstrap/Modal";
 
 import ListProjects from "./ProjectList";
 import DropDownButton from "./DropDown";
+import AddProjects from "./AddProject";
 
 const App = () => {
   //State that contain the project list
@@ -109,104 +108,6 @@ const App = () => {
           handleShow={handleShow}
           handleClose={handleClose}
         />
-      </div>
-    </div>
-  );
-};
-
-// Displays A Bootstrap Modal contains a form to submit form information
-const AddProjects = (props) => {
-  //references to all the textboxes in the form
-  const projectIDRef = useRef(null);
-  const projectNameRef = useRef(null);
-  const projectDscrRef = useRef(null);
-  const projectStartRef = useRef(null);
-  const projectEndRef = useRef(null);
-
-  //When the form is submitted handle adding a new project to the list
-  const handleAdd = (e) => {
-    e.preventDefault();
-    console.log("Adding project");
-
-    //Create a new project using the textbox input
-    let newProject = {
-      projectIdentifier: projectIDRef.current.value,
-      projectName: projectNameRef.current.value,
-      description: projectDscrRef.current.value,
-      start_date: projectStartRef.current.value,
-      end_date: projectEndRef.current.value,
-    };
-
-    //toggle the form to be hidden
-    props.handleClose();
-    //add the project to the list
-    props.addProject(newProject);
-  };
-
-  //Return a bootstrap modal contianing a form to update the project list
-  return (
-    <div>
-      <div
-        className="modal show"
-        style={{ display: "block", position: "initial" }}
-      >
-        <Modal show={props.show} onHide={props.handleClose}>
-          <Modal.Header>
-            <Modal.Title>Add Project</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Form>
-              <Form.Group className="mb-3" controlId="formId">
-                <Form.Label>Project ID</Form.Label>
-                <Form.Control
-                  type="text"
-                  ref={projectIDRef}
-                  placeholder="e.g. 17"
-                />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formName">
-                <Form.Label>Project Name</Form.Label>
-                <Form.Control
-                  type="text"
-                  ref={projectNameRef}
-                  placeholder="e.g. SQL Database"
-                />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formDscr">
-                <Form.Label>Project Description</Form.Label>
-                <Form.Control
-                  type="text"
-                  ref={projectDscrRef}
-                  placeholder="e.g. Stores user data"
-                />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formStart">
-                <Form.Label>Project Start Date</Form.Label>
-                <Form.Control
-                  type="text"
-                  ref={projectStartRef}
-                  placeholder="YYYY-MM-DD HH:MM:SS"
-                />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formEnd">
-                <Form.Label>Project End Date</Form.Label>
-                <Form.Control
-                  type="text"
-                  ref={projectEndRef}
-                  placeholder="YYYY-MM-DD HH:MM:SS"
-                />
-              </Form.Group>
-            </Form>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={props.handleClose}>
-              Close
-            </Button>
-            <Button variant="primary" onClick={handleAdd}>
-              Add Project
-            </Button>
-          </Modal.Footer>
-        </Modal>
       </div>
     </div>
   );
